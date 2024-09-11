d94a00d0d26e1f0b1b18841afd47576041939981
Ardi Germenji <ardi.germenji@gmail.com>
2024-09-11 16:53:39 +0100

    adds lint-staged

--- app/(auth-pages)/forgot-password/page.tsx
+++ app/(auth-pages)/forgot-password/page.tsx
@@ -4,7 +4,6 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import Link from "next/link"
 import { forgotPasswordAction } from "../actions"
-import { SmtpMessage } from "../aler"
 
 export default function ForgotPassword({
   searchParams,
@@ -32,7 +31,6 @@ export default function ForgotPassword({
           <FormMessage message={searchParams} />
         </div>
       </form>
-      <SmtpMessage />
     </>
   )
 }
